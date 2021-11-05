9f383b0ff4d5d4d946e81933551d05ddc84f34d1
Rootdiv <rootdiv@mail.ru>
2021-11-05 17:16:37 +0300

    feat: day one. Аuthorization form

---
+++ js/main.js
@@ -0,0 +1,60 @@
+'use strict';
+
+//День 1
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.getElementById('logInForm');
+const inputLogin = document.getElementById('login');
+const inputPassword = document.getElementById('password');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+const toggleModalAuth = () => {
+  modalAuth.classList.toggle('is-open');
+  inputLogin.removeAttribute('style');
+};
+
+const login = user => {
+  if (user.login.trim() !== '') {
+    logInForm.reset();
+    userName.textContent = user.login;
+    userName.style.display = 'inline';
+    buttonOut.style.display = 'block';
+    buttonAuth.style.display = 'none';
+    modalAuth.classList.remove('is-open');
+  } else {
+    inputLogin.style.cssText = 'border: 2px solid #ff0000';
+  }
+};
+
+const logout = () => {
+  userName.textContent = '';
+  userName.removeAttribute('style');
+  buttonOut.removeAttribute('style');
+  buttonAuth.removeAttribute('style');
+  localStorage.removeItem('gloDeliveryJS');
+};
+buttonOut.addEventListener('click', logout);
+
+logInForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const user = {
+    login: inputLogin.value,
+    password: inputPassword.value,
+  };
+  localStorage.setItem('gloDeliveryJS', JSON.stringify(user));
+  login(user);
+});
+
+buttonAuth.addEventListener('click', toggleModalAuth);
+closeAuth.addEventListener('click', toggleModalAuth);
+modalAuth.addEventListener('click', event => {
+  if (event.target.matches('.is-open')) {
+    toggleModalAuth();
+  }
+});
+
+if (localStorage.getItem('gloDeliveryJS')) {
+  login(JSON.parse(localStorage.getItem('gloDeliveryJS')));
+}
